83ca867ed8dd44fe8ddcaabf0393ee0dc7618c35
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-03-30 17:33:21 +0000

    código

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from Ejercicio_3 import Ejercicio_3
 
 def main():
   while True:
-    
-    
-
-yin = Yin() 
-yang = Yang() 
-yin.yang = yang 
-print(yang is yin.yang)
-del(yang)
-del(yin.yang)
-print(yang)
+    Ejercicio = input("Número del ejercicio que quiera iniciar")
+    if Ejercicio == Ejercicio_1:
+      ciudad = input("¿Que ciudad va a destruir?")
+      if ciudad == Nueva_York:
+        print("Nueva York ha sido destruida, así como los edificios A y B")
+      if ciudad == Los_Angeles:
+        print("No ha habido cataclismo en Los Ángeles")
+    if Ejercicio == Ejercicio_2:
+      yin = Yin() 
+      yang = Yang() 
+      yin.yang = yang 
+      print(yang is yin.yang)
+      del(yang)
+      del(yin.yang)
+      print(yang)
